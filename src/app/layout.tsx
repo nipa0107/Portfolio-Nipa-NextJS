4c3a4b6ce1913655eab272972421f868144403a6
minenipa <nipa.s@kkumail.com>
2025-07-17 23:59:01 +0700

    edit name file logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import "./globals.css";
 const inter = Inter({ subsets: ['latin'] })
@@ -7,11 +8,11 @@ import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Portfolio",
-  description: "Portfolio ของนักพัฒนาเว็บ",
+  description: "Portfolio By Nipa Sinthaisong",
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
   },
 };
 
@@ -22,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      </Head>
       <body className={`${inter.className}`}>
         <Header />
         <main className="container mx-auto px-8 py-8">
